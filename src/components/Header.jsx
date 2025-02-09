480bed9d7258452d2b98c6705190a6e0a7709666
Umair <umairshah0711@gmail.com>
2025-02-09 17:51:27 +0530

    navbar changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,16 @@ import React from "react";
 // fixed-top bg-transparent shadow
 const Header = () => {
   return (
- <></>
+    <nav className="bg-light ">
+      <div className="rounded-pill fixed-top shadow p-3 text-center my-5 d-flex justify-content-around w-50 mx-auto"
+       >
+        <span className="underline-on-hover">About</span>
+        <span className="underline-on-hover">Portfolio</span>
+        <span className="underline-on-hover">Services</span>
+        <span className="underline-on-hover">Blog</span>
+        <span className="underline-on-hover">Contact</span>
+      </div>
+    </nav>
   );
 };
 
